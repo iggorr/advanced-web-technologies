1d193a9c44fae5f607c604a778d865a026127282
Igors Ahmetovs <40125689@live.napier.ac.uk>
2017-10-11 17:52:01 +0000

    Added code from Chapter 3

--- hello.py
+++ hello.py
@@ -22,5 +22,20 @@ def private():
 def login():
   return "Please log in babe"
 
+@app.errorhandler(404)
+def page_not_found(error):
+  return "Couldn't find the page omg", 404
+
+@app.route('/force404/')
+def force404():
+  abort(404)
+
+@app.route('/static-example/img')
+def static_example_img():
+  start = '<img src="'
+  url = url_for('static', filename='vmask.jpg')
+  end = '">'
+  return start+url+end, 200
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
