bb907917d295ec5c50826fe7f61cddbe08a3c093
Igors Ahmetovs <40125689@live.napier.ac.uk>
2017-10-11 17:17:06 +0000

    Working through the workbook

--- hello.py
+++ hello.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
+def root():
+  return "Root route"
+
+@app.route("/hello")
 def hello():
-  return "Hello World!"
+  return "Hello Napier"
+
+@app.route("/goodbye/")
+def goodbye():
+  return "Aw leaving so soon"
+
+@app.route("/private/")
+def private():
+  #Test if logged in failed
+  return redirect(url_for('login'))
+
+@app.route("/login/")
+def login():
+  return "Please log in babe"
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0')
+  app.run(host='0.0.0.0', debug=True)
