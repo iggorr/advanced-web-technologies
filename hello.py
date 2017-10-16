333b94ef3ab074578b33145ba2a6c9adc2166636
Igors Ahmetovs <40125689@live.napier.ac.uk>
2017-10-16 19:54:29 +0000

    Added Chapter 4 Practical Examples

--- hello.py
+++ hello.py
@@ -1,13 +1,17 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
 @app.route("/")
 def root():
   return "Root route"
 
-@app.route("/hello")
+@app.route("/hello/")
 def hello():
-  return "Hello Napier"
+  name = request.args.get('name', '')
+  if name == '':
+    return "Hello Napier"
+  else:
+    return "Hello %s" % name
 
 @app.route("/goodbye/")
 def goodbye():
@@ -36,6 +40,60 @@ def static_example_img():
   url = url_for('static', filename='vmask.jpg')
   end = '">'
   return start+url+end, 200
+
+@app.route('/req/', methods=['GET', 'POST'])
+def req():
+  print request.method, request.path, request.form
+  return "Request info in logs bich"
+
+@app.route('/account/', methods=['GET', 'POST'])
+def account():
+  if request.method == 'POST':
+    print request.form
+    name = request.form['name']
+    return "Hello %s" % name
+  else:
+    page ='''
+    <html><body>
+      <form action="" method="post" name="form">
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name"/>
+        <input type="submit" name="suibmit" id="submit"/>
+      </form>
+      </body></html>'''
+
+    return page
+
+@app.route("/hi/<name>")
+def hi(name):
+  return "Hi %s" % name
+
+@app.route("/add/<int:first>/<int:second>")
+def add(first, second):
+  return str(first+second)
+
+@app.route("/display/")
+def display():
+  return '<img src="'+url_for('static', filename='uploads/file.png')+'"/>'
+
+@app.route("/upload/", methods=['POST', 'GET'])
+def upload():
+  if request.method == 'POST':
+    f = request.files['datafile']
+    f.save('static/uploads/file.png')
+    return "File Uploaded!"
+  else:
+    page='''
+    <html>
+    <body>
+    <form action="" method="post" name="form" enctype="multipart/form-data">
+      <input type="file" name="datafile" />
+      <input type="submit" name="submit" id="submit"/>
+    </form>
+    </body>
+    </html>
+    '''
+    return page, 200
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
